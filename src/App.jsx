543de63921efb2010a402c44539483bf653b0668
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-09-09 21:31:17 +0530

    Start front-end for 'AGRI SATHI HUB'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    //all content center using tailwindcss
+    <div className="w-full text-center p-20">
+      <h1 className='text-3xl font-bold underline'>AGRI SATHI HUB</h1>
+    </div>
+  ) 
+}
+
+export default App  
