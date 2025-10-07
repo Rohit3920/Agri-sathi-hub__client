5d07c82a3f99b0b63a7eec09ef26a3a9aa8e5f6b
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2025-10-07 18:50:58 +0530

    integrate dark/light mode and also change language component to change website language

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import './components/language/i18n'
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
 
   return (
     //all content center using tailwindcss
     <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors text-center">
       <Navbar />
-      <h1 className='text-3xl font-bold underline text-black dark:text-gray-300'>AGRI SATHI HUB</h1>
+      <h1 className='text-3xl font-bold underline text-black dark:text-gray-300'>{t('agri sathi hub')}</h1>
     </div>
   )
 }
